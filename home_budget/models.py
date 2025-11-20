0616f2d55bf5deff58c0425ec0f2ae6baeff2efd
garell1 <galkowski.mateusz@outlook.com>
2025-11-20 21:08:17 +0100

    add forms and html files

--- home_budget/models.py
+++ home_budget/models.py
@@ -2,19 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
     
 
 class Subcategory(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     category = models.ForeignKey(Category)
     
     def __str__(self):
         return f"{self.name} - {self.category}"
     
+
 class Expense(modsels.Model):
     category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
     subcategory = models.ForeignKey(Subcategory, related_name='subcategory', on_delete=models.CASCADE)
@@ -26,6 +27,7 @@ class Expense(modsels.Model):
     def __str__(self):
         return f"{self.month} {self.category}: {self.subcategory} {self.provide_amount} {self.real_amount}"
     
+
 class Income(models.Model):
     category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
     subcategory = models.ForeignKey(Subcategory, related_name='subcategory', on_delete=models.CASCADE)
