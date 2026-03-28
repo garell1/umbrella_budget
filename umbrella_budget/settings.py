d98ed31abf8675c00eed9e7a2d6bb012c3263260
garell1 <galkowski.mateusz@outlook.com>
2026-03-28 22:19:21 +0100

    add plugins

--- umbrella_budget/settings.py
+++ umbrella_budget/settings.py
@@ -51,7 +51,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home_budget',
+    'tailwind',
+    'theme',
 ]
+
+if DEBUG:
+    INSTALLED_APPS += ['django_browser_reload']
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
